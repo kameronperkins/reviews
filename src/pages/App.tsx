dc6e62d8012eb657773b964e29c50927d04c2e54
Kameron Perkins <perkins.kameron@gmail.com>
2021-09-21 20:00:23 -0600

    add default route

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,7 +3,7 @@ import {
   ChakraProvider,
   theme,
 } from "@chakra-ui/react"
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import Reviews from "./Reviews"
 import { QueryClient, QueryClientProvider } from "react-query"
 import Navigation from "../components/Navigation"
@@ -18,6 +18,9 @@ export const App = () => (
       <Router>
         <Navigation />
         <Switch>
+          <Route exact path='/'>
+            <Redirect to='/home' />
+          </Route>
           <Route path='/home' component={Home} />
           <Route path='/plays' component={Plays} />
           <Route path='/reviews' component={Reviews} />
